1c2c672ca1a88d7d4debbfb5791fbfe61252e852
qmannn <maksim.vesnin.tl@yandex.ru>
2017-05-15 22:22:58 +0400

    Добавлен файл установки службы Windows

---
+++ ServiceInstalling/install.bat
@@ -0,0 +1,8 @@
+::Остановка службы, если она запущена
+sc stop FtpProxyService
+:: Удаление, если она существует
+sc delete FtpProxyService
+
+::Установка
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil.exe %~dp0\FtpProxyService.exe
+pause
