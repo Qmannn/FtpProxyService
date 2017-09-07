010355f318ea8cb593855e5619606639cbb55dee
maksim.vesnin <maksim.vesnin@travelline.ru>
2017-09-07 23:35:58 +0300

    Поправил конфигурацию

--- Proxynet/App/AppConfig.ts
+++ Proxynet/App/AppConfig.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
     public static appBaseUrl: string = '/';
     public static routeBaseUrl: string = 'App/Pages';
-    public static appName: string = 'Proxynet';
-    public static resourceBaseUrl: string = '/Proxynet/service/';
+    public static appName: string = '';
+    public static resourceBaseUrl: string = '/service/';
 }
